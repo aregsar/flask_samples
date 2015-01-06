87a6a850194133341cbd907d71332b85c9166cc0
Areg Sarkissian <aregsar@gmail.com>
2015-01-06 14:07:41 -0800

    added app package

--- six/app/__init__.py
+++ six/app/__init__.py
@@ -1,13 +1,26 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# from app.views.home import home
-# app.register_blueprint(home)
-
-# from app.views.account import account
-# app.register_blueprint(account)
+from flask import Flask
+app = Flask(__name__)
 
 
-# app.config['DATABASE_URL'] = "postgres"
+from app.views.home import home
+app.register_blueprint(home)
 
-# print app.url_map
+from app.views.account import account
+app.register_blueprint(account)
+
+# from app.views import home
+# app.register_blueprint(home.home)
+
+# from app.views import account
+# app.register_blueprint(account.account)
+
+# from app.views import home
+# app.register_blueprint(home.res)
+
+# from app.views import account
+# app.register_blueprint(account.res)
+
+
+app.config['DATABASE_URL'] = "postgres"
+
+print app.url_map
